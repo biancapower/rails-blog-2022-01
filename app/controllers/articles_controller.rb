fd2ab5ad67be624eb48f4637473cac10d6e438a7
biancapower <bianca@biancapower.com>
2022-05-09 12:06:29 +1000

    add new and create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,13 @@ class ArticlesController < ApplicationController
 		@time = Time.now
 		@articles = Article.all
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(title: "This is a great lesson!", body: "Breakfast is the most important meal of the day", importance: 9)
+		@article.save
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
