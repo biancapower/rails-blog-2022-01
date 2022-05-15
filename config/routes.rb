4022038c7f479a07bf849f612abf58dd1ac48d18
biancapower <bianca@biancapower.com>
2022-05-15 16:34:56 +1000

    add a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get 'articles', to: 'articles#index'
   # get 'articles/new', to: 'articles#new'
+  root 'articles#index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
