c273a1bf64c0077efd5dd495734a5cae35fd2db7
DESKTOP-4J3791P\Orange <chen.zhang.62@hotmail.com>
2022-05-25 20:58:47 +1000

    action text added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_rich_text :body
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  # has_rich_text :body
 end
