6f659d35d31bb57d087deaa75ff18da7c26eb278
biancapower <bianca@biancapower.com>
2022-05-30 11:26:09 +1000

    update seed file to use a loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-articles = Article.create([
-	{ 
-		title: 'Star Wars',
-		body: 'Laborum in mollit cupidatat dolore pariatur anim eiusmod dolore dolor sit laborum Lorem adipisicing cillum. Cupidatat ut deserunt in velit amet proident nulla ea exercitation. Aliqua eu nisi enim irure culpa Lorem commodo occaecat in fugiat adipisicing.',
-		importance: 6
-	},
-	{ 
-		title: 'Star Trek',
-		body: 'Enim occaecat tempor dolore labore cillum qui non in sint. Excepteur aliquip in Lorem qui incididunt laborum ut voluptate ex. Eu ipsum amet aliquip mollit reprehenderit amet Lorem aliquip magna ipsum fugiat ea eu. Ipsum minim ad nulla voluptate sunt exercitation. In officia ea in occaecat Lorem non veniam dolor nulla. Est in amet fugiat veniam.',
-		importance: 2
-	}
-])
+10.times do |i|
+
+	Article.create([
+		{ 
+			title: "This is title #{i}",
+			body: 'Laborum in mollit cupidatat dolore pariatur anim eiusmod dolore dolor sit laborum Lorem adipisicing cillum. Cupidatat ut deserunt in velit amet proident nulla ea exercitation. Aliqua eu nisi enim irure culpa Lorem commodo occaecat in fugiat adipisicing.',
+			importance: rand(1..10)
+		}
+	])
+
+end
